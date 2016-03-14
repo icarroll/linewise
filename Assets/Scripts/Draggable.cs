494b1e87d7a66c1361ed90b2e632cad83633630c
Jon <jonhandy@gmail.com>
2016-03-13 22:10:09 -0600

    I don't really need a message, do I?

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	
+	public Transform parentToReturnTo = null;
+	public Transform placeholderParent = null;
+
+	GameObject placeholder = null;
+	
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("OnBeginDrag");
+		
+		//placeholder = new GameObject();
+		placeholder = Instantiate(this.gameObject);     //experiment
+		placeholder.GetComponent<CanvasRenderer>().SetColor(Color.gray);
+
+		placeholder.transform.SetParent( this.transform.parent );
+
+		placeholder.transform.localScale = this.gameObject.transform.localScale;  //experiment
+		LayoutElement le = placeholder.AddComponent<LayoutElement>();
+		le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+		le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+		le.flexibleWidth = 0;
+		le.flexibleHeight = 0;
+
+		placeholder.transform.SetSiblingIndex( this.transform.GetSiblingIndex() );
+		
+		parentToReturnTo = this.transform.parent;
+		placeholderParent = parentToReturnTo;
+		this.transform.SetParent( this.transform.parent.parent );
+		
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+	
+	public void OnDrag(PointerEventData eventData) {
+		//Debug.Log ("OnDrag");
+		
+		this.transform.position = eventData.position;   //Sets this game object to follow the mouse while dragging
+
+		if(placeholder.transform.parent != placeholderParent)	//Makes sure the placeholder object is linked with originating playspace						
+			placeholder.transform.SetParent(placeholderParent);
+
+		int newSiblingIndex = placeholderParent.childCount;
+
+		for(int i=0; i < placeholderParent.childCount; i++) {
+			if(this.transform.position.x < placeholderParent.GetChild(i).position.x) {
+
+				newSiblingIndex = i;
+
+				if(placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+					newSiblingIndex--;
+
+				break;
+				//TODO Create "Ghost" of card being dragged
+			}
+		}
+
+		placeholder.transform.SetSiblingIndex(newSiblingIndex);
+
+	}
+	
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("OnEndDrag");
+		this.transform.SetParent( parentToReturnTo );
+		this.transform.SetSiblingIndex( placeholder.transform.GetSiblingIndex() );
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+		Destroy(placeholder);
+	}
+	
+	
+	
+}
---
+++ Assets/Scripts/MenuChooser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuChooser : MonoBehaviour {
+
+	public void ChangeScene(string scenePath) {
+		SceneManager.LoadScene (scenePath);
+	}
+
+	public void QuitRequest(){
+		Application.Quit();
+	}
+		
+
+}
